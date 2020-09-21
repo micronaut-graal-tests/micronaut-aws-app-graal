d8be089a476b54d7fdd031ecaf6b5f6459317516
Iván López <lopezi@objectcomputing.com>
2020-09-21 12:30:03 +0200

    Add BintrayClient and controller

---
+++ src/main/java/micronaut/example/BintrayController.java
@@ -0,0 +1,21 @@
+package micronaut.example;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.util.List;
+
+@Controller("/bintray")
+public class BintrayController {
+
+    private final BintrayClient bintrayClient;
+
+    public BintrayController(BintrayClient bintrayClient) {
+        this.bintrayClient = bintrayClient;
+    }
+
+    @Get("/packages")
+    List<BintrayPackage> packages() {
+        return bintrayClient.fetchPackages();
+    }
+}
